f02633280155db2a0b5d91062ac9422982388531
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-12 01:10:00 +0800

    delete preinstall.sh in aflplusplus/, add pathfuzzerreduction/*

---
+++ fuzzers/pathfuzzerreduction/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+if [ ! -d "$FUZZER/repo" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+cd "$FUZZER/repo"
+bash compile_and_install_in_docker.sh
+mkdir -p "$OUT/afl" "$OUT/cmplog"
+
+
