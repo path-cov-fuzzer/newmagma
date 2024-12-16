7692488175f65a27ca5910e8905fa5819c2e2219
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-16 20:59:02 +0800

    recover target libtiff

--- targets/libtiff/fetch.sh
+++ targets/libtiff/fetch.sh
@@ -5,20 +5,11 @@
 # - env TARGET: path to target work dir
 ##
 
-# git clone --no-checkout https://gitlab.com/libtiff/libtiff.git \
-#     "$TARGET/repo"
-# git -C "$TARGET/repo" checkout c145a6c14978f73bb484c955eb9f84203efcb12e
-# 
-# cp "$TARGET/src/tiff_read_rgba_fuzzer.cc" \
-#     "$TARGET/repo/contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc"
+git clone --no-checkout https://gitlab.com/libtiff/libtiff.git \
+    "$TARGET/repo"
+git -C "$TARGET/repo" checkout c145a6c14978f73bb484c955eb9f84203efcb12e
 
-# git clone --no-checkout https://gitlab.com/libtiff/libtiff.git \
-#     "./repo"
-# git -C "./repo" checkout c145a6c14978f73bb484c955eb9f84203efcb12e
-# 
-# cp "./src/tiff_read_rgba_fuzzer.cc" \
-#     "./repo/contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc"
-
-mv $TARGET/fetched_repo $TARGET/repo
+cp "$TARGET/src/tiff_read_rgba_fuzzer.cc" \
+    "$TARGET/repo/contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc"
 
 
