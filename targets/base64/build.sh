8975f89c466d7ccdcf4391a85a284a4f830c040e
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-16 22:29:20 +0800

    add build.sh in LAVAM targets

---
+++ targets/base64/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $TARGET/repo
+autoreconf -f -i
+# make clean &> /dev/null
+make clean
+# ./configure --prefix=`pwd`/lava-install LIBS="-lacl" &> /dev/null
+./configure --prefix=`pwd`/lava-install LIBS="-lacl"
+# make -j $(nproc) &> /dev/null
+make -j $(nproc)
+
+cd ..
+
