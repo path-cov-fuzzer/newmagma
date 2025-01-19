4237b2e4bffb1c32cf3c4dc218daa09e95f4fbbe
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-01-19 09:23:48 +0800

    update

---
+++ magma/rungcc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env SHARED: path to directory shared with host (to store results)
+# - env PROGRAM: name of program to run (should be found in $OUT)
+# - env ARGS: extra arguments to pass to the program
+# - env FUZZARGS: extra arguments to pass to the fuzzer
+# - env POLL: time (in seconds) to sleep between polls
+# - env TIMEOUT: time to run the campaign
+# - env MAGMA: path to Magma support files
+# + env LOGSIZE: size (in bytes) of log file to generate (default: 1 MiB)
+##
+
+# set default max log size to 1 MiB
+LOGSIZE=${LOGSIZE:-$[1 << 20]}
+
+export MONITOR="$SHARED/monitor"
+mkdir -p "$MONITOR"
+
+# change working directory to somewhere accessible by the fuzzer and target
+cd "$SHARED"
+
+mkdir -p $SHARED/afl
+mkdir -p $SHARED/cmplog
+cp $OUT/afl/$PROGRAM $SHARED/afl/$PROGRAM
+cp $OUT/cmplog/$PROGRAM $SHARED/cmplog/$PROGRAM
+
+echo "Campaign terminated at $(date '+%F %R')"
+
+kill $(jobs -p)
