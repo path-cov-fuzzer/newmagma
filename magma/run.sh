41e8f7bffe549aa9cefa396ef0e7277af4d9a6d5
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-02-01 00:55:37 +0800

    for debug

--- magma/run.sh
+++ magma/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ##
 # Pre-requirements:
@@ -22,7 +23,9 @@ export MONITOR="$SHARED/monitor"
 mkdir -p "$MONITOR"
 
 # WHATWEADD: filter CFG for pathfuzzer
+echo "========================== out of this branch ==================="
 if [ "$FUZZER" == *"fixversion"* ]; then
+    echo "========================== in this branch ==================="
     (
         # copy everything of /magma_out/afl to /magma_shared/afl
         cp -r "$OUT/afl" "$SHARED/afl"
